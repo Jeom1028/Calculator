fe01bf610f2d2ff273514d1b04ec6de539f2ba7d
Jeom1028 <apam144@gmail.com>
2024-06-07 09:31:14 +0900

    ./.

--- Lv3/main.swift
+++ Lv3/main.swift
@@ -1,28 +1,28 @@
-class Calculator {
+class AbstractOperation {
     func execute(_ a: Double, _ b: Double) -> Double {
         fatalError("This method should be overridden by subclasses")
     }
 }
 
-class AddOperation: Calculator {
+class AddOperation: AbstractOperation {
     override func execute(_ a: Double, _ b: Double) -> Double {
         return a + b
     }
 }
 
-class SubtractOperation: Calculator {
+class SubtractOperation: AbstractOperation {
     override func execute(_ a: Double, _ b: Double) -> Double {
         return a - b
     }
 }
 
-class MultiplyOperation: Calculator {
+class MultiplyOperation: AbstractOperation {
     override func execute(_ a: Double, _ b: Double) -> Double {
         return a * b
     }
 }
 
-class DivideOperation: Calculator {
+class DivideOperation: AbstractOperation {
     override func execute(_ a: Double, _ b: Double) -> Double {
         if b == 0 {
             return 0
@@ -32,7 +32,7 @@ class DivideOperation: Calculator {
     }
 }
 
-class CalculatorClient {
+class Calculator {
     
     private let addOperation = AddOperation()
     private let subtractOperation = SubtractOperation()
@@ -53,7 +53,7 @@ class CalculatorClient {
     }
 }
 
-let calculatorClient = CalculatorClient()
+let calculator = Calculator()
 
 func getUserInput() -> (Double, Double, String)? {
     print("첫번째 숫자: ", terminator: "")
@@ -74,16 +74,16 @@ func getUserInput() -> (Double, Double, String)? {
     return (a, b, operation)
 }
 
-func performCalculation(calculatorClient: CalculatorClient, a: Double, b: Double, operation: String) -> Double {
+func performCalculation(calculator: Calculator, a: Double, b: Double, operation: String) -> Double {
     switch operation {
     case "+":
-        return calculatorClient.add(a, b)
+        return calculator.add(a, b)
     case "-":
-        return calculatorClient.subtract(a, b)
+        return calculator.subtract(a, b)
     case "*":
-        return calculatorClient.multiply(a, b)
+        return calculator.multiply(a, b)
     case "/":
-        return calculatorClient.divide(a, b)
+        return calculator.divide(a, b)
     default:
         fatalError("오류")
     }
@@ -95,6 +95,6 @@ while true {
         continue
     }
     
-    let result = performCalculation(calculatorClient: calculatorClient, a: a, b: b, operation: operation)
+    let result = performCalculation(calculator: calculator, a: a, b: b, operation: operation)
     print("Result: \(result)")
 }
